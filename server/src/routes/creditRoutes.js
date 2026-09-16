ab53618b405c608a31ec33074c61e2ea0acd1ae3
Alfaaz Website <alfaaz.website@gmail.com>
2026-09-17 00:07:39 +0530

    fix: resolve import error in creditRoutes to fix Render build crash

--- server/src/routes/creditRoutes.js
+++ server/src/routes/creditRoutes.js
@@ -1,12 +1,12 @@
 'use strict';
 const express = require('express');
 const { getCreditHistory, getAllTransactions, adminAdjustCredits } = require('../controllers/creditController');
-const { protect, restrictTo } = require('../middlewares/auth');
+const { protect, requireAdmin } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.get('/history', protect, getCreditHistory);
-router.get('/all', protect, restrictTo('admin'), getAllTransactions);
-router.post('/adjust', protect, restrictTo('admin'), adminAdjustCredits);
+router.get('/all', protect, requireAdmin, getAllTransactions);
+router.post('/adjust', protect, requireAdmin, adminAdjustCredits);
 
 module.exports = router;
